cacb98dd1a10e13776a8f719b72795405b79feb0
Blue-Lambda-University <justin.weber@bluelambdatechnologies.com>
2025-06-07 12:04:58 -0400

    chged

--- backend/v1/app/api/server.py
+++ backend/v1/app/api/server.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello Ota and Caroline!"}
+
+
+@app.get("/dashboard")
+def read_root():
+    return {"message": "This is My Dashboard!"}
