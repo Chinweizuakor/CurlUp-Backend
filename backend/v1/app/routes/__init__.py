ae9c8e4ce4f4e2927b3d2bebf1fac5f510af8158
Abraham Ajibade <97408939+jibbs1703@users.noreply.github.com>
2025-06-28 13:20:26 -0400

    refactored error with vendor routes and models

--- backend/v1/app/routes/__init__.py
+++ backend/v1/app/routes/__init__.py
@@ -8,6 +8,6 @@ from backend.v1.app.routes.vendor import router as vendor_router
 
 router = APIRouter()
 
-router.include_router(client_router)
-router.include_router(system_router)
-router.include_router(vendor_router)
+router.include_router(client_router, prefix="/client")
+router.include_router(system_router, prefix="/system")
+router.include_router(vendor_router, prefix="/vendor")
--- backend/v1/app/routes/vendor.py
+++ backend/v1/app/routes/vendor.py
@@ -2,11 +2,11 @@
 
 from fastapi import APIRouter
 
-from backend.v1.app.models.client import StylistCreate, StylistPublic
+from backend.v1.app.models.vendor import VendorCreate, VendorPublic
 
 router = APIRouter()
 
 
-@router.post("/register", response_model=StylistPublic, tags=["Vendor"])
-async def register(new_user: StylistCreate) -> StylistPublic:
-    return StylistPublic(**new_user.model_dump())
+@router.post("/register", response_model=VendorPublic, tags=["Vendor"])
+async def register(new_user: VendorCreate) -> VendorPublic:
+    return VendorPublic(**new_user.model_dump())
